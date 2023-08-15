0f8a72a283f8ab5193340a36f12ab761ad74d232
shuvra442 <116904738+shuvra442@users.noreply.github.com>
2023-08-16 01:29:06 +0530

    Add files via upload

---
+++ chatBot/src/main/java/com/java/chatBot/dto/ChatGPTResponse.java
@@ -0,0 +1,26 @@
+package com.java.chatBot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatGPTResponse {
+
+    private List<Choice> choices;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Choice {
+
+        private int index;
+        private Message message;
+
+    }
+}
